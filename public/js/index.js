45800862ecd0acef3a5b087bf6bd3c22a366247a
John Mateer <jj.mateer@live.com>
2019-10-12 19:03:27 -0700

    Changed about me section sizing.

--- public/js/index.js
+++ public/js/index.js
@@ -4,45 +4,36 @@ $("#topPointerBtn").on('click', function () {
     },
         1000);
 });
-$("#projectScroll" ).on('click', function () {
+$("#projectScroll, #projectScrollSM").on('click', function () {
+    document.getElementById("navMenuSM").style.display = "none";
     $('body').animate({
         scrollTop: $("#projectsHeader").offset().top
     },
         1000);
 });
-$("#contactScroll").on('click', function () {
+$("#contactScroll, #contactScrollSM").on('click', function () {
+    document.getElementById("navMenuSM").style.display = "none";
     $('body').animate({
         scrollTop: $("#contactCont").offset().top
     },
         1000);
 });
-$("#resourceScroll").on('click', function () {
+$("#resourceScroll, #resourceScrollSM").on('click', function () {
+    document.getElementById("navMenuSM").style.display = "none";
     $('body').animate({
         scrollTop: $("#resourcesCont").offset().top
     },
         1000);
 });
-$("#projectScrollSM" ).on('click', function () {
-    $('body').animate({
-        scrollTop: $(".regularCont").offset().top
-    },
-        1000);
-            document.getElementById("navMenuSM").style.display = "none";
-});
-$("#contactScrollSM").on('click', function () {
-    $('body').animate({
-        scrollTop: $("#contactCont").offset().top
-    },
-        1000);
-            document.getElementById("navMenuSM").style.display = "none";
-});
-$("#resourceScrollSM").on('click', function () {
-    $('body').animate({
-        scrollTop: $("#resourcesCont").offset().top
-    },
-        1000);
-            document.getElementById("navMenuSM").style.display = "none";
-});
+$("#TSNInfo").on("click", function () {
+    $("#TSNI.projectDesc").show();
+})
+$("#EDBInfo").on("click", function () {
+    $("#EDBI.projectDesc").show();
+})
+$("#GSInfo").on("click", function () {
+    $("#GSI.projectDesc").show();
+})
 $("#contactForm").on("submit", function () {
     event.preventDefault();
     $.post("/send", {
@@ -57,11 +48,11 @@ $("#contactForm").on("submit", function () {
             .val()
             .trim()
     }).then(function () {
-        $("#name").val(' ')
-        $("#email").val(' ')
-        $("#message").val(' ')
+        $("#name").val('')
+        $("#email").val('')
+        $("#message").val('')
     });
-});
+})
 const glideConfig = {
     type: "carousel",
     perView: 3,
@@ -75,8 +66,7 @@ const glideConfig = {
     }
 };
 new Glide('.glide', glideConfig).mount()
-
-$("#navMenu").on("click", function() {
+$("#navMenu").on("click", function () {
     document.getElementById("navMenuSM").style.display = "block";
 })
 function closeNav() {
